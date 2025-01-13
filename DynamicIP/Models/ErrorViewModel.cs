3c206313ddb94a05deca6400aed26df0aceb97f0
Tui Tên Tuấn <minhtuan9231@gmail.com>
2025-01-13 23:59:21 +0700

    initialize mvc

---
+++ DynamicIP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
